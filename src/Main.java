1505dd1c5872c31f4e18d95ef78f01d7c5762891
Konradek <konradbiznesu@gmail.com>
2020-11-02 00:06:40 +0100

    Labels, stages, scenes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.*;
+import javafx.scene.control.Label; // <- important for get child
+import java.awt.*;
 
 public class Main extends  Application {
 
@@ -10,8 +18,42 @@ public class Main extends  Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
         stage.setTitle("First App");
         stage.show();
+
+        Stage stage2 = new Stage();
+        stage2.setTitle("Second app");
+        stage2.setWidth(400);
+        stage2.setHeight(300);
+
+        stage2.setX(50);
+        stage2.setY(100);
+
+        VBox parent = new VBox();
+
+        Label label1 = new Label("This is label");
+        Label label2 = new Label("Second label");
+        parent.getChildren().addAll(label1,label2);
+
+        Scene scene1 = new Scene(parent);
+        scene1.setCursor(Cursor.CROSSHAIR);
+        stage2.setScene(scene1);
+
+
+        stage2.show();
+
+        /*
+        Stage newWindow = new Stage();
+        newWindow.initModality(Modality.APPLICATION_MODAL);
+        newWindow.show();
+        */
+
+        Stage newWindow1 = new Stage();
+        newWindow1.initStyle(StageStyle.DECORATED);
+        newWindow1.initOwner(stage2);
+        newWindow1.initModality(Modality.WINDOW_MODAL);
+        newWindow1.show();
 
     }
 
@@ -22,7 +64,7 @@ public class Main extends  Application {
 
     public static void main(String[] args) {
 
-       launch();
+       launch(args);
 
     }
 }
