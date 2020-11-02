eb8047e65aae93591b594ca1a7962f669b9b2f56
Konradek <konradbiznesu@gmail.com>
2020-11-02 14:25:19 +0100

    Simply Register

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
@@ -26,17 +23,35 @@ public class Main extends  Application {
     public void start(Stage stage) throws Exception {
 
 
-        stage.setTitle("BordePane");
-        stage.setWidth(400);
-        stage.setHeight(400);
 
-        //Label label = new Label("Elo420");
-        ImageView img = new ImageView("https://static-cdn.jtvnw.net/jtv_user_pictures/fximba-profile_image-8779ecca7c5d7fb1-300x300.png");
-        Button button = new Button("Click me hehe");
-        StackPane root = new StackPane(img,button);
+        stage.setTitle("GridPaneane");
+        stage.setHeight(250);
+        stage.setWidth(250);
 
-//        StackPane.setAlignment(button,Pos.CENTER_LEFT);
-        StackPane.setMargin(button,new Insets(30));
+        Label label = new Label("user");
+        Label label1 = new Label("email");
+        Label label2 = new Label("psswd");
+
+        TextField textField = new TextField();
+        TextField textField1 = new TextField();
+        PasswordField textField2 = new PasswordField();
+
+        Button button = new Button("Utwórz");
+
+        GridPane root = new GridPane();
+
+        //kolumna, wiersz
+        root.add(label, 0, 0);
+        root.add(label1, 0, 1);
+        root.add(label2, 0, 2);
+        root.add(textField,1,0);
+        root.add(textField1,1,1);
+        root.add(textField2,1,2);
+        root.add(button,1,3);
+        //customize
+        root.setVgap(15);
+        root.setHgap(10);
+        root.setPadding(new Insets(60));
 
 
         Scene scene = new Scene(root);
