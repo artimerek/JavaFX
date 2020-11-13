623fe54d3656cfe2a27a6c6b7b0d4a88c36f1901
Konradek <konradbiznesu@gmail.com>
2020-11-13 14:32:33 +0100

    set center method

--- MyLibrary/src/main/java/pl/mylibrary/controllers/TopMenuButtonsController.java
+++ MyLibrary/src/main/java/pl/mylibrary/controllers/TopMenuButtonsController.java
@@ -2,24 +2,40 @@ package pl.mylibrary.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
     private MainController mainController;
 
     @FXML
+    private ToggleGroup toggleButtons;
+
+    @FXML
     public void openLibrary() {
+        mainController.setCenter("/fxml/Library.fxml");
     }
 
     @FXML
     public void openListBooks() {
+        mainController.setCenter("/fxml/ListBooks.fxml");
     }
 
     @FXML
     public void openStats() {
+        mainController.setCenter("/fxml/Stats.fxml");
     }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
+    }
+
+    @FXML
+    public void addBook() {
+        if(toggleButtons.getSelectedToggle()!=null)
+            toggleButtons.getSelectedToggle().setSelected(false);
+
+        mainController.setCenter("/fxml/AddBook.fxml");
     }
 }
