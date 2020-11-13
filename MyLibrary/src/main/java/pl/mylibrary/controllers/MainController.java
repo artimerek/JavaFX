22e2e8bce2b181f87e5fa2c6b3e8af8f8250c35c
Konradek <konradbiznesu@gmail.com>
2020-11-13 13:20:15 +0100

    Top buttons added to main BorderPane

--- MyLibrary/src/main/java/pl/mylibrary/controllers/MainController.java
+++ MyLibrary/src/main/java/pl/mylibrary/controllers/MainController.java
@@ -1,4 +1,11 @@
 package pl.mylibrary.controllers;
 
+import javafx.fxml.FXML;
+
 public class MainController {
+
+    @FXML
+    private TopMenuButtonsController topMenuButtonsController;
+
+
 }
---
+++ MyLibrary/src/main/java/pl/mylibrary/controllers/TopMenuButtonsController.java
@@ -0,0 +1,19 @@
+package pl.mylibrary.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class TopMenuButtonsController {
+
+    @FXML
+    public void openLibrary() {
+    }
+
+    @FXML
+    public void openListBooks() {
+    }
+
+    @FXML
+    public void openStats() {
+    }
+}
