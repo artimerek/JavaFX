de0439e4ed7630bb16016e6bb13196a2196a4d47
artimerek <konradbiznesu@gmail.com>
2021-09-06 23:52:42 +0200

    Making my first attempt to connect db with java by jdbc

--- JavaFXwithJDBC/src/pl/artimerek/Main.java
+++ JavaFXwithJDBC/src/pl/artimerek/Main.java
@@ -1,8 +1,21 @@
 package pl.artimerek;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       /* Connecting to db and creating statement instance for using sql's.
+          Connection is automatically closed cause of try from version 8 of java
+       */
+	    try(Connection connection = DriverManager.getConnection("jdbc:sqlite:.\\mydb.db");
+            Statement statement = connection.createStatement()){
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
